a37e4b258c2f2470463a5e6540660eec60db9e1f
johgee <graceeunaejoh@grace.com>
2022-03-06 12:41:56 -0500

    including time app

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -1,5 +1,26 @@
 class ExamplePagesController < ApplicationController
-  def hello_method
-    render json: {message: "hello!"}
+  def greetings_method
+    render json: {m1: "hello!", m2: "how are you doing?", m3: "good"}
+
   end 
+
+  def how_method
+    render json: {message: "how are you doing?"}
+  end 
+
+  def bye_method
+    render json: {message: "goodbye!"}
+  end
+
+  def store_method
+    render html: "<h1>This is my HTML</h1><p>This is my site</p>".html_safe
+  end
+
+  def time
+    render html: "<h2>It is 11:32!</h2>".html_safe
+  end
+
+  def time_method
+    render json: {message: "Welcome to the time app", time: Time.now. strftime("%e %b %Y %H:%M:%S%p")}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/hello_path", controller: "example_pages", action: "hello_method"
-end
+  get "/greetings", controller: "example_pages", action: "greetings_method" 
+  get "/how_path", controller: "example_pages", action: "how_method"
+  get "/bye_path", controller: "example_pages", action: "bye_method" 
+  get "/store", controller: "example_pages", action: "store_method" 
+  get "/time", controller: "example_pages", action: "time_method" 
+end 
